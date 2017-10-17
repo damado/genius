25c26194f0b5647cd9bdc54e54293b63d8f3a59e
Damian <amadodamian2@gmail.com>
2017-10-16 23:12:48 -0300

    agrego contacto

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ContactoService } from '../../services/contacto.service';
 
 @Component({
   selector: 'app-contact',
@@ -9,24 +10,25 @@ import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
 export class ContactComponent implements OnInit {
 
 	public formulario: FormGroup;
+  private arrayChecks: Array<string> = [];
 
-  constructor(protected fb: FormBuilder) { }
+  constructor(protected fb: FormBuilder, protected contactoService: ContactoService) { }
 
   ngOnInit() {
   	this.formulario = this.fb.group({
   		'nombre' : ['', [Validators.required]],
   		'mail' : ['', [Validators.required, Validators.email]],
   		'numero_telefono' : [''],
-  		'vacaciones' : [''],
-  		'escapada_finde' : [''],
-  		'casamiento' : [''],
-  		'compras' : [''],
-  		'recuerdos_amigos' : [''],
-  		'sueño_viajar' : [''],
-  		'playa' : [''],
-  		'deporte_nieve' : [''],
-  		'crucero' : [''],
-  		'descubrir' : [''],
+      'vacaciones' : [false],
+      'escapada_finde' : [false],
+      'casamiento' : [false],
+      'compras' : [false],
+      'recuerdos_amigos' : [false],
+      'sueño_viajar' : [false],
+      'playa' : [false],
+      'deporte_nieve' : [false],
+      'crucero' : [false],
+      'descubrir' : [false],
   		'cantidad_dias' : [''],
       'fecha_viaje' : [''],
   		'cantidad_personas' : [''],
@@ -35,8 +37,30 @@ export class ContactComponent implements OnInit {
   	});
   }
 
+  /**
+  *  Envia al servicio de contacto la informacion ingresada para contactarse con la agencia.
+  **/
   enviarFormulario(): void {
-  	console.log(this.formulario.value);
+    const nombre = this.formulario.get('nombre').value;
+    const mail = this.formulario.get('mail').value;
+    const numero_telefono = this.formulario.get('numero_telefono').value;
+    const cantidad_dias = this.formulario.get('cantidad_dias').value;
+    const fecha_viaje = this.formulario.get('fecha_viaje').value;
+    const cantidad_personas = this.formulario.get('cantidad_personas').value;
+    const motivo_contacto = this.formulario.get('motivo_contacto').value;
+    const comentarios = this.formulario.get('comentarios').value;
+    this.contactoService.contactarAgencia(nombre, mail, numero_telefono, this.arrayChecks, cantidad_dias, fecha_viaje, cantidad_personas, motivo_contacto, comentarios);
+  }
+
+  /**
+  *  Agrega un valor a la lista de checks, o lo elimina si el elemento ya se encuentra.
+  **/
+  agregarCheck(nameControl: string, value: string): void {
+    if (!this.formulario.get(nameControl).value) {
+      this.arrayChecks.push(value);
+      return;
+    }
+    this.arrayChecks.splice(this.arrayChecks.indexOf(value), 1);
   }
 
 }
--- src/app/services/contacto.service.ts
+++ src/app/services/contacto.service.ts
@@ -9,14 +9,35 @@ export class ContactoService {
 
   constructor(private http: Http) { }
 
-  contactarAgencia() {
-  	const body = '';
+  /**
+  *  Se envia un post con los datos ingresados para enviarselo a la agencia.
+  **/
+  contactarAgencia(nombre: string, email:string, numero_telefono: string, arrayChecks: Array<string>, cantidad_dias:string, fecha_viaje: string, cantidad_personas: string, motivo_contacto: string, comentarios: string) {
+    const body = this.formatearJsonBody(nombre, email, numero_telefono, arrayChecks, cantidad_dias, fecha_viaje, cantidad_personas, motivo_contacto, comentarios);
+    console.log(body);
   	const headers = new Headers({'Content-Type': 'application/json'});
   	const options = new RequestOptions({headers: headers});
-  	this.http.options(this.url + 'suscriptores');
-  	this.http.post(this.url + 'suscriptores', body, options).subscribe(response => {
+    this.http.options(this.url + 'contacto');
+    this.http.post(this.url + 'contacto', body, options).subscribe(response => {
   		return response;
   	});
+  }
+
+  /**
+  *  Le da formato al request para enviar la informacion de contacto a la agencia.
+  **/
+  formatearJsonBody(nombre: string, email:string, numero_telefono: string, arrayChecks: Array<string>, cantidad_dias:string, fecha_viaje: string, cantidad_personas: string, motivo_contacto: string, comentarios: string) {
+    return JSON.stringify({
+      'nombre': nombre,
+      'email': email,
+      'numero_telefono': numero_telefono,
+      'checks': arrayChecks,
+      'cantidad_dias': cantidad_dias,
+      'fecha_viaje': fecha_viaje,
+      'cantidad_personas': cantidad_personas,
+      'motivo_contacto': motivo_contacto,
+      'comentarios': comentarios,
+    });
   }
 
 }
